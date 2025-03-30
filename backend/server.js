7d52b7b540d968a8e736c97af1403e67e62af928
Madhav <madhav0570@gmail.com>
2025-03-31 00:08:57 +0530

    naming updated

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
 
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
