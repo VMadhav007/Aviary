b6058800a03bdc4f186204eb0b35b2ad4a95dc72
Madhav <madhav0570@gmail.com>
2025-04-03 00:31:36 +0530

    UI added

---
+++ frontend/src/components/svgs/x.jsx
@@ -0,0 +1,7 @@
+const AbstractBirdSvg = (props) => (
+    <svg aria-hidden="true" viewBox="0 0 24 24" fill="white" {...props}>
+      <path d="M4 10c5-5 11-8 16-6-3 2-6 4-8 7 3 0 6 1 9 2-2 3-5 6-9 8-2 1-5 1-7-1 3-2 5-3 7-5-3-1-6-2-9-3z" />
+    </svg>
+  );
+  export default AbstractBirdSvg;
+  
