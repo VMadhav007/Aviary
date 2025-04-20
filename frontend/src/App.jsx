e0f44066746fa14922b8b6cb8165edc8a9bd154d
Madhav <madhav0570@gmail.com>
2025-04-20 10:45:27 +0530

    integration left

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
 import NotificationPage from './pages/notification/NotificationPage';
 import ProfilePage from './pages/profile/ProfilePage';
+import { Toaster } from 'react-hot-toast';
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
@@ -19,6 +20,7 @@ function App() {
 				<Route path='/profile/:username' element={<ProfilePage/>} />
 			</Routes>
       <RightPanel />
+	  <Toaster/>
 		</div>
 	);
 }
